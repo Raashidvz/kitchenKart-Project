ede8e7f3a252494953eb1021b6dacc53bbe5601b
Raashidvz <raashidvz313@gmail.com>
2025-07-11 17:59:57 +0530

    Update settings.py

--- kitchenKart/kitchenKart/settings.py
+++ kitchenKart/kitchenKart/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kitchenkart_db',
         'USER' : 'root',
-        'PASSWORD' : 'MYSQLpass@1234',
+        'PASSWORD' : '',
         'HOST' :'localhost',
         'PORT' :'3306',
         'OPTIONS': {
